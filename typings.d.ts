27b17503689bb41242962ad2c1a1185fc24d25cf
komfysach <sach@getkomfy.net>
2022-11-09 11:33:32 +0100

    updated typings

--- typings.d.ts
+++ typings.d.ts
@@ -24,7 +24,7 @@ export interface PageInfo extends SanityBody {
   _type: 'pageInfo';
   address: string;
   email: string;
-  heroImage: string;
+  heroImage: Image;
   name: string;
   phoneNumber: string;
 }
