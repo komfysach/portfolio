455bddadb4ab640530b0e473b292245b473bef4f
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2023-10-12 11:44:23 +0200

    check revalidation

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -1,5 +1,6 @@
 import {Project} from '../typings'
 
+export const revalidate = 1;
 export const fetchProjects = async () => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`)
 
