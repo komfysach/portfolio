a7029a0f70b3eeaa0c2df19fc3526ebf429658a2
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2023-10-12 12:34:24 +0200

    remove unused import

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -3,7 +3,6 @@ import {motion} from 'framer-motion'
 import { Project } from '../typings'
 import { urlFor } from '../sanity'
 import Link from 'next/link'
-import Image from 'next/image'
 
 type Props = {
   project: Project
